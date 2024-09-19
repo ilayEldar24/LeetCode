79a55a535b4ca3dba2c5f2257eab76867ab90b36
Ilay Eldar <105712778+ilayEldar24@users.noreply.github.com>
2024-09-19 08:10:01 +0300

    Time: 885 ms (9.82%), Space: 27.4 MB (45.38%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hset = set()
+
+        for num in nums:
+            if num in hset:
+                return True
+            else:
+                hset.add(num)
+        return False
