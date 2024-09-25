babcdb04ffecf8410138290022ff2bb6976660a3
Ilay Eldar <105712778+ilayEldar24@users.noreply.github.com>
2024-09-25 12:47:23 +0300

    Time: 31 ms (63.76%), Space: 19.4 MB (35.51%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,24 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
-    counter = 0
-    number = -1
     def kthSmallest(self, root, k):
-        def preOrder(root, k):
+        self.counter = 0  # Use instance variables instead of class variables
+        self.number = -1
+
+        def inOrder(root, k):
             if not root:
                 return
-            preOrder(root.left, k)
+            inOrder(root.left, k)
             self.counter += 1
             if self.counter == k:
                 self.number = root.val
-            preOrder(root.right, k)
-        preOrder(root,k)
+                return
+            inOrder(root.right, k)
+
+        inOrder(root, k)
         return self.number
-    
-            
-            
-        
