bf38fa437f9b903484327e1631068f15fda5b4f4
Ilay Eldar <105712778+ilayEldar24@users.noreply.github.com>
2024-09-26 18:41:03 +0300

    Time: 21 ms (35.04%), Space: 13.4 MB (99.24%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,15 +3,18 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution(object):
     def reverseList(self, head):
-        prev, cur = None, head
+        if not head:
+            return None
+        prev = head
+        cur = head.next
+        prev.next = None
         
         while cur:
             nxt = cur.next
             cur.next = prev
-            prev = cur  
+            prev = cur
             cur = nxt
         
-        return prev  
+        return prev
