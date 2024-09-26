1deb6b039550642ff6592f888ec1f91fcdc5bc86
Ilay Eldar <105712778+ilayEldar24@users.noreply.github.com>
2024-09-26 17:41:59 +0300

    Time: 11 ms (93.09%), Space: 11.4 MB (99.62%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,24 +1,23 @@
+
 class Solution(object):
     def removeElement(self, nums, val):
-        def swap(arr, l, r):
-            tmp = arr[l]
-            arr[l] = arr[r]
-            arr[r] = tmp
-
-        l = 0
-        r = len(nums) - 1
-        while (l < r):
-            if nums[l] == val:
-                if nums[r] != val:
-                    swap(nums, l, r)
-                    r -= 1
-                    l += 1
-                elif nums[r] == val:
-                    r -= 1
+        l , r = 0, len(nums) - 1
+        
+        if not nums:
+            return 0
+        
+        while l < r:
+            if nums[r] == val:
+                    r-=1
             else:
-                l += 1
-                
-        for i in range(len(nums)):
-            if nums[i] == val:
-                nums = nums[:i]
-                return i
+                if nums[l] == val:
+                    tmp = nums[l]
+                    nums[l] = nums[r]
+                    nums[r] = tmp
+                    r-=1
+                l+=1
+        
+        if nums[l] == val:
+            return l
+        else:
+            return l+1
